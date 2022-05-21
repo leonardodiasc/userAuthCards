3a388aee6c025b14ff1da750a3bc9a1e6728c9b0
leonardodiasc <leonardodiass@gmail.com>
2022-05-21 13:06:42 -0300

    The web app now displays some quotes at "/"

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+var app = express();
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+
+
+app.use(session({
+  secret: 'marmelada de banana bananada de caramelo',
+  resave: true,
+  saveUninitialized: false,
+   store: MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/bookworm'
+  })
+}));
+
+// use sessions for tracking logins
+
+
+//make user ID available for the templates
+app.use(function(req,res,next){
+  res.locals.currentUser = req.session.userId;
+  next();
+});
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// include routes
+var routes = require('./routes/index');
+const cardRoutes = require('./routes/cards');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
+});
---
+++ routes/index.js
@@ -0,0 +1,150 @@
+var express = require('express');
+const router = express.Router();
+var User = require('../models/user');
+var mid = require('../middleware');
+
+//GET profile
+
+router.get('/profile', mid.requiresLogIn, function(req, res, next){
+  if (!req.session.userId){
+    var err = new Error("Viewing this page requires authentication.");
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+      .exec(function(error, user){
+        if(error){
+          return next(error);
+        }else {
+          return res.render('profile', {title: 'Profile', name: user.name, favorite:
+          user.favoriteBook});
+        }
+      });
+});
+
+//GET log out
+router.get('/logout', function(req,res,next){
+  if(req.session){
+    req.session.destroy(function(err){
+      if(err){
+        return next(err);
+      } else{
+        return res.redirect('/');
+      }
+    });
+  }
+});
+// GET and POST login
+router.get('/login', mid.loggedOut,function(req,res,next){
+  return res.render('login',{ title: 'Log In'});
+});
+router.post('/login', function(req,res,next){
+  if(req.body.email && req.body.password){
+    User.authenticate(req.body.email, req.body.password, function (error, user){
+      if(error || !user){
+        var err = new Error('Something is wrong');
+        err.status = 401;
+        return next(err);
+      }else{
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  }else{
+    var err = new Error ('The fields email and password are necessary.');
+    err.status = 401;
+    return next(err);
+  }
+});
+// GET / register
+router.get('/register', mid.loggedOut, function(req,res,next){
+  return res.render('register', { title: 'Sign up'});
+});
+
+//POST/ Register
+router.post('/register', (req,res,next)=>{
+  if (req.body.email &&
+      req.body.name &&
+      req.body.favoriteBook &&
+      req.body.password &&
+      req.body.confirmPassword){
+        if(req.body.password !== req.body.confirmPassword){
+          var err = new Error('Passwords not matching, retype them.');
+          err.status = 400;
+          return next(err);
+        }
+        var userInfo = {
+          email: req.body.email,
+          name: req.body.name,
+          favoriteBook: req.body.favoriteBook,
+          password: req.body.password
+        }
+        //inserting the user information 'userInfo' into mongodb
+        User.create(userInfo, (error, user)=>{
+          if (error){
+            return next(error);
+          } else {
+            req.session.userId = user._id;
+            return res.redirect('/profile');
+          }
+        });
+      } else{
+        var err = new Error('Fill all fields, dear one.');
+        err.status = 400;
+        return next(err);
+      }
+});
+// GET /
+//router.get('/', function(req, res, next) {
+//  return res.render('index', { title: 'Home' });
+//});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+// FlashCard information
+
+const  data  = require('../data/flashcardData.json').data;
+const  cards  = data.cards;
+
+// Flashcard routes
+
+router.get( '/', ( req, res ) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/${flashcardId}` )
+});
+
+router.get('/:id', (req, res) => {
+    const side  = req.query.side;
+    const  id  = req.params.id;
+
+    if ( !side ) {
+        return res.redirect(`/${id}?side=question`);
+    }
+    const text = cards[id][side];
+    const hint  = cards[id].hint;
+
+    const templateData = { id, text, side };
+
+    if ( side === 'question' ) {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+      templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = 'Question';
+    }
+
+    res.render('card', templateData);
+});
+
+
+module.exports = router;
